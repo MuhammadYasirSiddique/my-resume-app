f1b5eff975b3d0018f36c2e744a874ad66da8618
MuhammadYasir <imyasir79@yahoo.com>
2024-10-05 16:54:49 +0500

    Succes! loading UI improved.

--- src/app/auth/reset-password/From.tsx
+++ src/app/auth/reset-password/From.tsx
@@ -4,6 +4,8 @@ import React, { useState, ChangeEvent, FormEvent } from "react";
 import { useRouter } from "next/navigation";
 import { Eye, EyeOff } from "lucide-react"; // Importing icons from lucide-react
 import { useSession } from "next-auth/react";
+import { LoaderCircle } from "lucide-react";
+import toast from "react-hot-toast";
 
 const ResetPasswordPage = () => {
   const [formData, setFormData] = useState({
@@ -11,33 +13,34 @@ const ResetPasswordPage = () => {
     password: "",
     confirmPassword: "",
   });
-  const [error, setError] = useState<string | null>(null);
-  const [success, setSuccess] = useState<boolean>(false);
+
+  const [showOldPassword, setShowOldPassword] = useState<boolean>(false); // State to toggle password visibility
   const [showPassword, setShowPassword] = useState<boolean>(false); // State to toggle password visibility
   const [showConfirmPassword, setShowConfirmPassword] =
     useState<boolean>(false); // For confirm password field
   const router = useRouter();
   const { data: session } = useSession();
+  const [loading, setLoading] = useState<boolean>(false);
 
   // Handle input changes
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
-    setError(null);
   };
 
   // Handle form submission
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    setLoading(true);
 
     if (!session || !session.user || !session.user.email) {
-      setError("Unauthorized");
+      toast.error("Unauthorized");
       return;
     }
 
     const { oldpassword, password, confirmPassword } = formData;
 
     if (password !== confirmPassword) {
-      setError("Passwords do not match!");
+      toast.error("Passwords do not match!");
       return;
     }
 
@@ -50,20 +53,21 @@ const ResetPasswordPage = () => {
       });
 
       if (response.status === 400) {
-        setError("Invalid old password");
+        toast.error("Invalid old password");
         return;
       }
       if (response.ok) {
-        setSuccess(true);
-
+        toast.success("Password reset successful");
         setTimeout(() => {
           router.push("/dashboard");
         }, 2000);
       } else {
-        setError("Failed to reset password. Please try again.");
+        toast.error("Failed to reset password. Please try again.");
       }
     } catch (err) {
-      setError("An error occurred. Please try again." + err);
+      toast.error("An error occurred. Please try again." + err);
+    } finally {
+      setLoading(false);
     }
   };
 
@@ -87,7 +91,7 @@ const ResetPasswordPage = () => {
             </label>
             <div className="flex items-center mt-1">
               <input
-                type={showPassword ? "text" : "password"} // Toggle between text and password
+                type={showOldPassword ? "text" : "password"} // Toggle between text and password
                 name="oldpassword"
                 id="oldpassword"
                 required
@@ -99,9 +103,9 @@ const ResetPasswordPage = () => {
               <button
                 type="button"
                 className="absolute right-3 text-gray-500 hover:text-gray-700 focus:outline-none"
-                onClick={() => setShowPassword(!showPassword)} // Toggle visibility
+                onClick={() => setShowOldPassword(!showOldPassword)} // Toggle visibility
               >
-                {showPassword ? (
+                {showOldPassword ? (
                   <EyeOff className="w-5 h-5" />
                 ) : (
                   <Eye className="w-5 h-5" />
@@ -175,18 +179,15 @@ const ResetPasswordPage = () => {
             </div>
           </div>
 
-          {error && <p className="text-red-600 text-sm">{error}</p>}
-          {success && (
-            <p className="text-green-600 text-sm">
-              Password reset successfully. Redirecting...
-            </p>
-          )}
-
           <button
             type="submit"
             className="w-full px-4 py-2 text-white bg-indigo-600 rounded-md shadow hover:bg-indigo-700 transition duration-300"
           >
-            Reset Password
+            {loading ? (
+              <LoaderCircle className="animate-spin h-5 w-5 mx-auto" />
+            ) : (
+              "Reset Password"
+            )}
           </button>
         </form>
       </div>
